1ff442993a4e42ede5e96b8017bf0125aa645729
karansahota736 <152522719+karansahota736@users.noreply.github.com>
2025-11-06 23:00:46 +0530

    fix server.js

--- server.js
+++ server.js
@@ -340,11 +340,11 @@ app.get("/api/messages/:user1/:user2", async (req, res) => {
 // const path = require("path");
 
 
-// app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 const PORT = process.env.PORT || 10000;
 
